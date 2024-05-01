5abfc59981c4bdee234b963e0434cd749946043e
juan <mirandajuan1998@outlook.com>
2024-05-01 07:54:07 -0300

    added screenshots

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a solution to the [Testimonials grid section challenge on Frontend Mento
 
 - [Overview](#overview)
 - [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
 - [Links](#links)
 - [My process](#my-process)
 - [Built with](#built-with)
@@ -19,6 +20,12 @@ This is a solution to the [Testimonials grid section challenge on Frontend Mento
 Users should be able to:
 
 - View the optimal layout for the site depending on their device's screen size
+
+
+### Screenshot
+
+![Desktop(1440px)](./capture/capture-desktop.png)
+![Mobile(375px)](./capture/capture-mobile.png)
 
 
 ### Links
--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             I received a job offer mid-course, and the subjects I learned were current, if not more so,
             in the company I joined. I honestly feel I got every penny’s worth.
           </p>
-          <p className='text-sm text-[#cecece] opacity-70'>
+          <p className='text-sm lg:text-base text-[#cecece] opacity-70'>
             “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
             transition and have heard some people who had an amazing experience here. I signed up
             for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
@@ -41,7 +41,7 @@ function App() {
             </div>
           </div>
           <p className='pr-3 text-xl tracking-wide leading-6 font-medium mb-4'>The team was very supportive and kept me motivated</p>
-          <p className='text-sm opacity-70'>
+          <p className='text-sm lg:text-base opacity-70'>
             “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
             for a big company. This was one of the best investments I’ve made in myself. ”
           </p>
@@ -58,7 +58,7 @@ function App() {
             </div>
           </div>
           <p className='pr-3 text-xl tracking-wide leading-6 font-medium mb-4'>An overall wonderful and rewarding experience</p>
-          <p className='text-sm opacity-70'>
+          <p className='text-sm lg:text-base opacity-70'>
             “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living
             while doing something I love. ”
           </p>
@@ -77,7 +77,7 @@ function App() {
             Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and
             learning from their experiences was easy.
           </p>
-          <p className='text-sm opacity-70'>
+          <p className='text-sm lg:text-base opacity-70'>
             “ The staff seem genuinely concerned about my progress which I find really refreshing. The program
             gave me the confidence necessary to be able to go out in the world and present myself as a capable
             junior developer. The standard is above the rest. You will get the personal attention you need from
@@ -95,7 +95,7 @@ function App() {
             </div>
           </div>
           <p className='pr-3 text-xl tracking-wide leading-6 font-medium mb-4'>Such a life-changing experience. Highly recommended!</p>
-          <p className='text-sm opacity-70'>
+          <p className='text-sm lg:text-base opacity-70'>
             “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from
             professionals who can help me learn programming step by step. I was encouraged to enroll by a former
             student of theirs who can only say wonderful things about the program. The entire curriculum and staff
